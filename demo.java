1c563342ace8f4fd32b8816b3730e103acb6b207
pradeepneha2468-pixel <pradeepneha2468@gmail.com>
2026-03-31 22:54:29 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,19 @@
+package JavaExamples;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+//        int a = 10;
+//        double b = a;  // automatic conversion
+//
+//        System.out.println(b);
+        double a = 10.75;
+        int b = (int) a;  // manual conversion
+
+        System.out.println(b);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
